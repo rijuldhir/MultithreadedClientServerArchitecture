52baa2206f64b467d5a582a1a57faa32d59acc68
rijul.dhir@gmail.com <rijul.dhir@gmail.com>
2018-04-08 14:50:21 +0530

    Server side Main Updated

--- Server.java
+++ Server.java
@@ -4,9 +4,24 @@ import java.net.Socket;
 // this class handles all the Server side architecture
 public class Server
 {
+	public static final int Port = 2737;
 	public static void main()
 	{
+		Scanner in = new Scanner(System.in);
+		while(true){
+		Socket cs = new Socket("localhost",Port);
+		//DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 		
+		BufferedReader inFromClient = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+		System.out.println("Enter Integer n and FileName");
+		int n = in.nextInt();
+		if(n==-1)
+			break;
+		String fileName = in.next();
+		Thread new_thread = new generateRequest(cs,fileName,n)
+		new_thread.start();
+		new_thread.join();
+		}
 	}
 }
 
@@ -14,15 +29,19 @@ public class Server
 class generateRequest extends Thread
 {
 	private Socket sock;
-	
-	public generateRequest(Socket sock) 
+	private String fileName;
+	private int n;
+	public generateRequest(Socket sock,String file,int n) 
 	{
 		this.sock = sock;
+		this.n=n;
+		this.fileName=file;
 	}
 	
 	@Override
 	public void run()
 	{
+		//PrintWriter outToClient = new PrintWriter(sock.getOutputStream(),true);
 		
 	}
 }
